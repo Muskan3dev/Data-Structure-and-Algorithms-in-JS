ca083a41675d8450c6997140dee61eb3267549a9
Muskan3dev <muskannovember3@gmail.com>
2023-08-14 23:52:09 +0530

    Adding 'Arrays' Data Structure

---
+++ Arrays.js
@@ -0,0 +1,13 @@
+//Arrays-An array is a data structure that stores a collection of elements. 
+//In JavaScript, arrays are objects that have a length property that indicates the number of elements in the array. The elements of an array can be accessed using indexes, which are integers that start at 0.
+
+//Traversing an array
+const array=[10,20,30,40,50]
+function traversearray(){
+    for(let i=0;i<array.length;i++){
+        console.log(arr[i])
+    }
+}
+
+console.log("Traversing array elements");
+traversearray(array);
