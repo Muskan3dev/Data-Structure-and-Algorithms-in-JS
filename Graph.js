0f6b898028900e4144548e85cdc1f25097240ab7
Muskan3dev <muskannovember3@gmail.com>
2023-09-17 22:08:22 +0530

    Adding vertex in graph

---
+++ Graph.js
@@ -0,0 +1,10 @@
+//Graphs
+//Adding vertex in graph
+class Graph{
+    constructor(){
+        this.adjacencyList = {};
+    }
+    addVertex(vertex){
+        if(!this.adjacencyList[vertex]) this.adjacencyList[vertex] = [];
+    }
+}
